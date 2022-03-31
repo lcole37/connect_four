9639fbd259082971eaa6fa7856845aa176ab2a59
Deannah Burke <deannah@bierstadtlager.com>
2022-03-30 19:40:40 -0600

    Update board.rb to print empty board(Iteration 1)

--- lib/board.rb
+++ lib/board.rb
@@ -1,25 +1,15 @@
-require 'pry'
 class Board
+  attr_reader :create_board, :welcome
 
-  # attr_accessor :board
-  attr_reader  :columns, :rows, :board
-
-  def initialize(columns = 7, rows = 6)
-    @columns = columns
-    @rows = rows
-    # @board = @create_board
+  def initialize
+    @create_board = "ABCDEF\n.......\n.......\n.......\n.......\n.......\n......."
+    @welcome = "Welcome to CONNECT FOUR!"
   end
 
-  def create_board
-    board = []
-    @columns.times do
-      board << Array.new(@rows)
-    end
-    board
+  def print_welcome
+    "#{welcome}\n\n" + "#{create_board}"
   end
 
 end
-
-board = Board.new
-# board.create_board
-# p board.create_board
+# board = Board.new
+# puts board.print_welcome
