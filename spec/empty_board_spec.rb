2075059c83059f835b32cb7ba01f03c8a19b5c27
Deannah Burke <deannah@bierstadtlager.com>
2022-03-31 14:21:28 -0600

    Move iteration 1 board spec to new file

---
+++ spec/empty_board_spec.rb
@@ -0,0 +1,24 @@
+require 'rspec'
+require './lib/empty_board'
+
+RSpec.describe Board do
+  it "exists" do
+    board = Board.new
+    expect(board).to be_an_instance_of(Board)
+  end
+
+  it "can create a board" do
+    board = Board.new
+    expect(board.create_board).to eq("ABCDEF\n.......\n.......\n.......\n.......\n.......\n.......")
+  end
+
+  it "can print a welcome message" do
+    board = Board.new
+    expect(board.welcome).to eq("Welcome to CONNECT FOUR!")
+  end
+
+  it "can display welcome message and empty board" do
+    board = Board.new
+    expect(board.print_welcome).to eq("Welcome to CONNECT FOUR!\n\nABCDEF\n.......\n.......\n.......\n.......\n.......\n.......")
+  end
+end
