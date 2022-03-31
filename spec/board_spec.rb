7df20311d952173715bca3acd3ef38b536536d41
Deannah Burke <deannah@bierstadtlager.com>
2022-03-30 19:41:41 -0600

    Update board spec (Iteration 1)

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -8,18 +8,17 @@ RSpec.describe Board do
   end
 
   it "can create a board" do
-      board = Board.new
-      expect(board.create_board).to eq([
-        [nil, nil, nil, nil, nil, nil],
-        [nil, nil, nil, nil, nil, nil],
-        [nil, nil, nil, nil, nil, nil],
-        [nil, nil, nil, nil, nil, nil],
-        [nil, nil, nil, nil, nil, nil],
-        [nil, nil, nil, nil, nil, nil],
-        [nil, nil, nil, nil, nil, nil]])
+    board = Board.new
+    expect(board.create_board).to eq("ABCDEF\n.......\n.......\n.......\n.......\n.......\n.......")
   end
-  #     board.create_board(rows)
-  #     expect(board.create_board).to eq(6)
-  #     #new expect containing columns, rows, from .times do method??
 
+  it "can print a welcome message" do
+    board = Board.new
+    expect(board.welcome).to eq("Welcome to CONNECT FOUR!")
+  end
+
+  it "can display welcome message and empty board" do
+    board = Board.new
+    expect(board.print_welcome).to eq("Welcome to CONNECT FOUR!\n\nABCDEF\n.......\n.......\n.......\n.......\n.......\n.......")
+  end
 end
