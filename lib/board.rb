8c5da653d3da0ce15e5abe2f3dfc7d682f6cf909
Lucas Colwell <colwellc37@gmail.com>
2022-04-01 14:12:23 -0600

    Feat: add theoretical board and test

--- lib/board.rb
+++ lib/board.rb
@@ -1,7 +1,7 @@
 class Board
   attr_reader :print_board, :welcome, :columns, :rows, :board
 
-  def initialize#(columns, rows)
+  def initialize(columns, rows)
     @print_board = "ABCDEF\n.......\n.......\n.......\n.......\n.......\n......."
     @welcome = "Welcome to CONNECT FOUR!"
     @columns = columns
@@ -13,19 +13,19 @@ class Board
     "#{welcome}\n\n" + "#{print_board}"
   end
 
+  def create_board
+    board = []
+    @columns.times do
+      board << Array.new(@rows)
+    end
+    board
+  end
+end
 
 #board.new
 #puts board.print_welcome
-#put these in main menu file to display welcome and empty board 
+#put these in main menu file to display welcome and empty board
 
-  def create_board
-     board = []
-     @columns.times do
-       board << Array.new(@rows)
-     end
-     board
-   end
-end
 #board = Board.new
 #board.create_board
 #p board.create_board
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -3,63 +3,34 @@ require './lib/board'
 
 RSpec.describe Board do
   it "exists" do
-    board = Board.new#(columns, rows)
+    board = Board.new(7, 6)
     expect(board).to be_an_instance_of(Board)
   end
 
   it "can create a empty board" do
-    board = Board.new
+    board = Board.new(7, 6)
     expect(board.print_board).to eq("ABCDEF\n.......\n.......\n.......\n.......\n.......\n.......")
   end
 
   it "can print a welcome message" do
-    board = Board.new#(columns, rows)
+    board = Board.new(7, 6)
     expect(board.welcome).to eq("Welcome to CONNECT FOUR!")
   end
 
   it "can display welcome message and empty board" do
-    board = Board.new#(columns, rows)
+    board = Board.new(7, 6)
     expect(board.print_welcome).to eq("Welcome to CONNECT FOUR!\n\nABCDEF\n.......\n.......\n.......\n.......\n.......\n.......")
   end
+
+  it "can create a board" do
+    board = Board.new(7, 6)
+    expect(board.create_board).to eq([
+      [nil, nil, nil, nil, nil, nil],
+      [nil, nil, nil, nil, nil, nil],
+      [nil, nil, nil, nil, nil, nil],
+      [nil, nil, nil, nil, nil, nil],
+      [nil, nil, nil, nil, nil, nil],
+      [nil, nil, nil, nil, nil, nil],
+      [nil, nil, nil, nil, nil, nil]])
+    end
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#Lucas' logic below
-# require 'rspec'
-# require './lib/board'
-#
-# RSpec.describe Board do
-#   it "exists" do
-#     board = Board.new
-#     expect(board).to be_an_instance_of(Board)
-#   end
-#
-#   it "can create a board" do
-#       board = Board.new
-#       expect(board.create_board).to eq([
-#         [nil, nil, nil, nil, nil, nil],
-#         [nil, nil, nil, nil, nil, nil],
-#         [nil, nil, nil, nil, nil, nil],
-#         [nil, nil, nil, nil, nil, nil],
-#         [nil, nil, nil, nil, nil, nil],
-#         [nil, nil, nil, nil, nil, nil],
-#         [nil, nil, nil, nil, nil, nil]])
-#   end
-#   #     board.create_board(rows)
-#   #     expect(board.create_board).to eq(6)
-#   #     #new expect containing columns, rows, from .times do method??
-#
-# end
