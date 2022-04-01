39d7424206dfa2850637fbe7233456361ae17648
Lucas Colwell <colwellc37@gmail.com>
2022-04-01 13:37:51 -0600

    Mess up and pull Deannah's

--- lib/board.rb
+++ lib/board.rb
@@ -1,24 +1,34 @@
 class Board
-  attr_reader :create_board, :welcome
+  attr_reader :print_board, :welcome, :rows, :columns, :board
 
-  def initialize
-    @create_board = "ABCDEF\n.......\n.......\n.......\n.......\n.......\n......."
-    @welcome = "Welcome to CONNECT FOUR!"
+  def initialize(columns, rows)
+    @print_board = "ABCDEF\n.......\n.......\n.......\n.......\n.......\n......."
+    @welcome = "Welcome to CONNECT FOUR!\n\n" + "ABCDEF\n.......\n.......\n.......\n.......\n.......\n......."
+    @columns = columns
+    @rows = rows
   end
 
   def print_welcome
-    "#{welcome}\n\n" + "#{create_board}"
-  end
+    "#{welcome}\n\n" + "#{print_board}"
 
+
+  def create_board
+    board = []
+    @columns.times do
+      board << Array.new(@rows)
+    end
+    board
+  end
 end
+#
+# end
+#
+# board = Board.new
+# # board.create_board
+# # p board.create_board
+
 # board = Board.new
 # puts board.print_welcome
-
-
-
-
-
-
 
 #Lucas' logic
 # require 'pry'
@@ -26,23 +36,3 @@ end
 #
 #   # attr_accessor :board
 #   attr_reader  :columns, :rows, :board
-#
-#   def initialize(columns = 7, rows = 6)
-#     @columns = columns
-#     @rows = rows
-#     # @board = @create_board
-#   end
-#
-#   def create_board
-#     board = []
-#     @columns.times do
-#       board << Array.new(@rows)
-#     end
-#     board
-#   end
-#
-# end
-#
-# board = Board.new
-# # board.create_board
-# # p board.create_board
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -3,24 +3,24 @@ require './lib/board'
 
 RSpec.describe Board do
   it "exists" do
-    board = Board.new
+    board = Board.new(7, 6)
     expect(board).to be_an_instance_of(Board)
   end
 
-  it "can create a board" do
-    board = Board.new
-    expect(board.create_board).to eq("ABCDEF\n.......\n.......\n.......\n.......\n.......\n.......")
-  end
+  # it "can print welcome board" do
+  #   board = Board.new(7, 6)
+  #   expect(board.print_board).to eq("ABCDEF\n.......\n.......\n.......\n.......\n.......\n.......")
+  # end
 
   it "can print a welcome message" do
-    board = Board.new
-    expect(board.welcome).to eq("Welcome to CONNECT FOUR!")
+    board = Board.new(7, 6)
+    expect(board.print_welcome).to eq("Welcome to CONNECT FOUR!\n\n ABCDEF\n.......\n.......\n.......\n.......\n.......\n.......")
   end
 
-  it "can display welcome message and empty board" do
-    board = Board.new
-    expect(board.print_welcome).to eq("Welcome to CONNECT FOUR!\n\nABCDEF\n.......\n.......\n.......\n.......\n.......\n.......")
-  end
+  # it "can display welcome message and empty board" do
+  #   board = Board.new(7, 6)
+  #   expect(board.print_welcome).to eq("Welcome to CONNECT FOUR!\n\nABCDEF\n.......\n.......\n.......\n.......\n.......\n.......")
+  # end
 end
 
 
@@ -37,7 +37,7 @@ end
 
 
 
-#Lucas' logic below 
+#Lucas' logic below
 # require 'rspec'
 # require './lib/board'
 #
