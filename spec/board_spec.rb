6ecbb169350e546bdf2767ee6fcd54e987f72cc8
Deannah Burke <deannah@bierstadtlager.com>
2022-03-31 18:46:30 -0600

    Combine Iteration 1 board spec files

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -8,18 +8,58 @@ RSpec.describe Board do
   end
 
   it "can create a board" do
-      board = Board.new
-      expect(board.create_board).to eq([
-        [nil, nil, nil, nil, nil, nil],
-        [nil, nil, nil, nil, nil, nil],
-        [nil, nil, nil, nil, nil, nil],
-        [nil, nil, nil, nil, nil, nil],
-        [nil, nil, nil, nil, nil, nil],
-        [nil, nil, nil, nil, nil, nil],
-        [nil, nil, nil, nil, nil, nil]])
+    board = Board.new
+    expect(board.create_board).to eq("ABCDEF\n.......\n.......\n.......\n.......\n.......\n.......")
   end
-  #     board.create_board(rows)
-  #     expect(board.create_board).to eq(6)
-  #     #new expect containing columns, rows, from .times do method??
 
+  it "can print a welcome message" do
+    board = Board.new
+    expect(board.welcome).to eq("Welcome to CONNECT FOUR!")
+  end
+
+  it "can display welcome message and empty board" do
+    board = Board.new
+    expect(board.print_welcome).to eq("Welcome to CONNECT FOUR!\n\nABCDEF\n.......\n.......\n.......\n.......\n.......\n.......")
+  end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Lucas' logic below 
+# require 'rspec'
+# require './lib/board'
+#
+# RSpec.describe Board do
+#   it "exists" do
+#     board = Board.new
+#     expect(board).to be_an_instance_of(Board)
+#   end
+#
+#   it "can create a board" do
+#       board = Board.new
+#       expect(board.create_board).to eq([
+#         [nil, nil, nil, nil, nil, nil],
+#         [nil, nil, nil, nil, nil, nil],
+#         [nil, nil, nil, nil, nil, nil],
+#         [nil, nil, nil, nil, nil, nil],
+#         [nil, nil, nil, nil, nil, nil],
+#         [nil, nil, nil, nil, nil, nil],
+#         [nil, nil, nil, nil, nil, nil]])
+#   end
+#   #     board.create_board(rows)
+#   #     expect(board.create_board).to eq(6)
+#   #     #new expect containing columns, rows, from .times do method??
+#
+# end
